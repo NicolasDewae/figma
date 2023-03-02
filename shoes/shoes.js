ee1bbb0a97d3a70b900285326c2cbe4a88ccd1ff
Nicolas De waegenaere <n.dewaegenaere@mesange-prevoyance.fr>
2023-03-02 11:15:23 +0100

    animation updated from css to js

--- shoes/shoes.js
+++ shoes/shoes.js
@@ -1,13 +1,69 @@
 const classMini = document.querySelector('.mini');
+const classMax = document.querySelector('.max');
+const classPro = document.querySelector('.pro');
 const classMen = document.querySelector('.men');
-const collections = document.querySelector('.collections');
+const classCollections = document.querySelector('.collections');
 
-classMini.addEventListener('mouseover', () => {
-    classMen.classList.add('navbarNewColor');
-    collections.classList.add('navbarNewColor');
+// animation for class .mini
+classMini.addEventListener('click', () => {
+    
+    // change the colour of the navbar from black to white or vice versa
+    classMen.classList.toggle('navbarNewColor');
+    classCollections.classList.toggle('navbarNewColor');
+
+    // rotate the image and reset other classes
+    classMini.classList.toggle('rotate');
+    classMax.classList.remove('rotate');
+    classPro.classList.remove('rotate');
+
+    // change the width
+    if (classMini.classList.contains('rotate') === true) {
+        classMini.style.width = "60%";
+        classMax.style.width = "20%";
+        classPro.style.width = "20%";
+    } else {
+        classMini.style.width = "calc(100% / 3)";
+        classMax.style.width = "calc(100% / 3)";
+        classPro.style.width = "calc(100% / 3)";
+    }
 });
 
-classMini.addEventListener('mouseout', () => {
+classMax.addEventListener('click', () => {
+    
     classMen.classList.remove('navbarNewColor');
-    collections.classList.remove('navbarNewColor');
+    classCollections.classList.remove('navbarNewColor');
+
+    classMax.classList.toggle('rotate');
+    classMini.classList.remove('rotate');
+    classPro.classList.remove('rotate');
+
+    if (classMax.classList.contains('rotate') === true) {
+        classMini.style.width = "20%";
+        classMax.style.width = "60%";
+        classPro.style.width = "20%";
+    } else {
+        classMini.style.width = "calc(100% / 3)";
+        classMax.style.width = "calc(100% / 3)";
+        classPro.style.width = "calc(100% / 3)";   
+    }
+});
+
+classPro.addEventListener('click', () => {
+    
+    classMen.classList.remove('navbarNewColor');
+    classCollections.classList.remove('navbarNewColor');
+
+    classPro.classList.toggle('rotate');
+    classMini.classList.remove('rotate');
+    classMax.classList.remove('rotate');
+
+    if (classPro.classList.contains('rotate') === true) {
+        classMini.style.width = "20%";
+        classMax.style.width = "20%";
+        classPro.style.width = "60%";
+    } else {
+        classMini.style.width = "calc(100% / 3)";
+        classMax.style.width = "calc(100% / 3)";
+        classPro.style.width = "calc(100% / 3)";   
+    }
 });
